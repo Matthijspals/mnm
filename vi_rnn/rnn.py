165d21db8d164575c70a4a6d015f109331ff5f12
Matthijs <matthijs@example.com>
2024-10-31 22:06:04 +0100

    cdm task

--- vi_rnn/rnn.py
+++ vi_rnn/rnn.py
@@ -260,7 +260,7 @@ class LRRNN(nn.Module):
             if z0 is None:
                 z = torch.randn(1, self.d_z, 1, 1, device=self.R_x.device)
             else:
-                if len(z0.squeeze().shape) == 1 and (self.d_s is None or self.d_s != 1):  # only z dimension is given
+                if (z0.shape[0] == 1 or len(z0.shape)==1) and (self.d_s is None or self.d_s != 1):  # only z dimension is given
                     z = z0.to(device=self.R_x.device).reshape(1, self.d_z, 1, 1)
                 elif len(z0.shape) < 4:  # trial and z dimension is given
                     z = z0.to(device=self.R_x.device).reshape(
