fix alpha comp when masking

vi_rnn/load_data.py
@@ -1,7 +1,8 @@
 import os 
 import pandas as pd
 import numpy as np
-from scipy.signal import convolve, gaussian, decimate
+from scipy.signal import convolve, decimate
+from scipy.signal.windows import gaussian
 
 def bin_spike_train(spike_times, t_start, t_end, bin_size):
     """

vi_rnn/vae.py
@@ -598,7 +598,7 @@ class VAE(nn.Module):
             precZ = 1 / eff_var_prior
             precE = 1 / Evar[:, :, t]
             precQ = precZ + precE * ed_mask[:,t].view(-1,1,1)
-            alpha = precE / precQ
+            alpha = precE / (precZ + precE)
             alphas.append(alpha)
             #mean_Q = (1 - alpha) * prior_mean + alpha * Emean[:, :, t]
             eff_var_Q = 1 / precQ